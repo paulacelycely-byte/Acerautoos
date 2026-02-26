d6b3d09292144e69ff471fcbc9bf7c7748286c8e
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2026-02-26 11:08:32 -0500

    se realizaron los ultimos cambios

--- sena/app/urls.py
+++ sena/app/urls.py
@@ -17,6 +17,9 @@ from .views.Proveedores.views import *
 from .views.tipo_servicio.views import *
 from .views.compra.views import *
 from .views.factura.views import *
+from .views.Salida_vehiculo.views import *
+from .views.Insumos.views import *
+from .views.Servicio.views import *
 # Agregamos esta importación para el Dashboard
 from .views.dashboard.views import DashboardView 
 
@@ -99,6 +102,29 @@ urlpatterns = [
     # --- MÓDULO DE FACTURAS (SOLO CONSULTA) ---
     path('factura/listar/', FacturaListView.as_view(), name='listar_facturas'),
     path('factura/detalle/<int:pk>/', FacturaDetailView.as_view(), name='detalle_factura'),
+    
+    
+    
+     # ===== SALIDA VEHÍCULO =====
+    path('listar_salida_vehiculo/', Salida_VehiculoListView.as_view(), name='listar_salida_vehiculo'),
+    path('crear_salida_vehiculo/', Salida_VehiculoCreateView.as_view(), name='crear_salida_vehiculo'),
+    path('editar_salida_vehiculo/<int:pk>/', Salida_VehiculoUpdateView.as_view(), name='editar_salida_vehiculo'),
+    path('eliminar_salida_vehiculo/<int:pk>/', Salida_VehiculoDeleteView.as_view(), name='eliminar_salida_vehiculo'),
+    
+    
+     # ===== INSUMOS =====
+    path('listar_insumo/', InsumoListView.as_view(), name='listar_insumo'),
+    path('crear_insumo/', InsumoCreateView.as_view(), name='crear_insumo'),
+    path('editar_insumo/<int:pk>/', InsumoUpdateView.as_view(), name='editar_insumo'),
+    path('eliminar_insumo/<int:pk>/', InsumoDeleteView.as_view(), name='eliminar_insumo'),
+    
+        # ===== SERVICIO =====  
+    path('listar_servicio/', ServicioListView.as_view(), name='listar_servicio'),
+    path('crear_servicio/', ServicioCreateView.as_view(), name='crear_servicio'),           
+    path('editar_servicio/<int:pk>/', ServicioUpdateView.as_view(), name='editar_servicio'),
+    path('eliminar_servicio/<int:pk>/', ServicioDeleteView.as_view(), name='eliminar_servicio'),
+    
+    
     
     # --- MÓDULO DE LOGIN ---
     path('acceso/', include(('login.urls', 'login'), namespace='login')),
