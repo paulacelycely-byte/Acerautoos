fdb3dfedcb33d31eb5d338353a9e295623f449fe
Angie Yuliana Pedraza Barrera <angiey_pedrazaq@soy.sena.edu.co>
2026-06-09 19:21:26 -0500

    se hicieron cambios es estilos de compra, compatibilidad y backups

--- sena/sena/settings.py
+++ sena/sena/settings.py
@@ -61,9 +61,9 @@ WSGI_APPLICATION = 'sena.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'proyecto_acerautos',
+        'NAME':     'proyecto',
         'USER':     'root',
-        'PASSWORD': 'Acerautos2026*',
+        'PASSWORD': '1234567890',
         'HOST':     'localhost',
         'PORT':     '3306',
         'OPTIONS': {
