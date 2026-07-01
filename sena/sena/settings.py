3e452346d52b95e31ecef916384d0f7c9013a625
Nereito <nereotorom@gmail.com>
2026-07-01 11:17:01 -0500

    se realizaron cambios

--- sena/sena/settings.py
+++ sena/sena/settings.py
@@ -62,9 +62,9 @@ WSGI_APPLICATION = 'sena.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'acerautos_proyecto',
+        'NAME':     'proyectooooo',
         'USER':     'root',
-        'PASSWORD': 'Acerautos2026*',
+        'PASSWORD': '12345678',
         'HOST':     'localhost',
         'PORT':     '3306',
         'OPTIONS': {
